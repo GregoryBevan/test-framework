6159597a9200d04ce8dc4ff33214092e233d339a
Grégory <grego@elgregos.com>
2014-10-21 22:36:45 +0200

    Repare arquillian test

--- src/test/java/com/elgregos/test/arquillian/wildfly/WildFlyCliInvokerTest.java
+++ src/test/java/com/elgregos/test/arquillian/wildfly/WildFlyCliInvokerTest.java
@@ -2,14 +2,17 @@ package com.elgregos.test.arquillian.wildfly;
 
 import java.io.File;
 
+import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Test wilfly cli invoke. Needs wildfly to be started
  *
  * @author Grégory
  */
+@RunWith(Arquillian.class)
 public class WildFlyCliInvokerTest {
 
 	@Test
