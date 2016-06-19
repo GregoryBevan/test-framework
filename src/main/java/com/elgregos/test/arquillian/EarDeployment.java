0dc7ea103f428b9810327574b29acbbc5154d783
Grégory <Grgory@ultra-GREGO>
2016-06-19 23:38:27 +0200

    Upgrade to last gradle version

--- src/main/java/com/elgregos/test/arquillian/EarDeployment.java
+++ src/main/java/com/elgregos/test/arquillian/EarDeployment.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jboss.arquillian.protocol.servlet.arq514hack.descriptors.api.web.WebAppDescriptor;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
@@ -11,8 +12,6 @@ import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.descriptor.api.Descriptors;
-import org.jboss.shrinkwrap.descriptor.api.ejbjar32.EjbJarDescriptor;
-import org.jboss.shrinkwrap.descriptor.api.webapp30.WebAppDescriptor;
 
 import com.elgregos.test.arquillian.resolver.ArtifactoryResolver;
 
@@ -30,7 +29,6 @@ public abstract class EarDeployment {
 
 	protected WebAppDescriptor webAppDescriptor = Descriptors.create(WebAppDescriptor.class);
 
-	protected EjbJarDescriptor ejbJarDescriptor = Descriptors.create(EjbJarDescriptor.class);
 
 	public EarDeployment(final String earName) {
 		this.enterpriseArchive = ShrinkWrap.create(EnterpriseArchive.class, earName);
@@ -38,7 +36,6 @@ public abstract class EarDeployment {
 		this.webAppDescriptor.version("3.1");
 		this.ejbModule = ShrinkWrap.create(JavaArchive.class, "service.jar");
 		this.ejbModule.addClass(DummyForEJBDeployment.class);
-		this.ejbJarDescriptor.version("3.2");
 		this.earLibraries = new ArrayList<>();
 		final JavaArchive testClassesJar = ShrinkWrap.create(JavaArchive.class, "test.jar")
 				.addClasses(EarDeployment.class, this.getClass().getEnclosingClass()).addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
--- src/main/java/com/elgregos/test/arquillian/wildfly/WildFlyCliInvoker.java
+++ src/main/java/com/elgregos/test/arquillian/wildfly/WildFlyCliInvoker.java
@@ -1,12 +1,13 @@
 package com.elgregos.test.arquillian.wildfly;
 
 import java.io.File;
+import java.net.InetAddress;
 import java.util.Scanner;
 
-import lombok.extern.slf4j.Slf4j;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.dmr.ModelNode;
 
-import org.jboss.as.cli.CommandContext;
-import org.jboss.as.cli.scriptsupport.CLI;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Invoke wildly command line interface
@@ -29,13 +30,12 @@ public class WildFlyCliInvoker {
 	}
 
 	public void processCliScript(final File file) {
-		final CLI cli = CLI.newInstance();
-		cli.connect();
-		final CommandContext commandContext = cli.getCommandContext();
 		try (final Scanner scanner = new Scanner(file)) {
+			ModelControllerClient client = ModelControllerClient.Factory.create(InetAddress.getByName("localhost"), 9999);
 			while (scanner.hasNextLine()) {
 				final String command = scanner.nextLine();
-				commandContext.handle(command);
+				ModelNode op = new ModelNode(command);
+				client.execute(op);
 			}
 		} catch (final Throwable th) {
 			log.error("An error occurred while processing CLI script", th);
